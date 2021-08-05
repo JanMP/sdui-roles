e86823ebbc42b5523d20e1652bd419939374cedf
JanMP <pille@mac.com>
2021-08-05 08:33:51 +0200

    handover

--- sdui-roles.js
+++
@@ -1,3 +0,0 @@
-export {userWithIdIsInRole, currentUserIsInRole, useCurrentUserIsInRole, currentUserMustBeInRole} from './imports/roleChecks'
-export {default as LoginForm} from './imports/ui/LoginForm'
-export {default as SetPasswordForm} from './imports/ui/SetPasswordForm'
